61f2ce7e6c1f5e69a3233a0f26717ea2aad5fa6e
RodolfoNieblas <37163935+RodolfoNieblas@users.noreply.github.com>
2020-05-02 16:02:59 -0700

    stuff

--- Untitled_Horror_Game/Assets/Scripts/Main Menu/placeholder.cs
+++ Untitled_Horror_Game/Assets/Scripts/Main Menu/placeholder.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMoveTest : MonoBehaviour
+public class placeholder : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
