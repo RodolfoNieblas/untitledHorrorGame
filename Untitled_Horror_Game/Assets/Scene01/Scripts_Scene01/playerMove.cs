518234bfbd8f6f26a2e5b29ff063d129a8d6d326
RodolfoNieblas <37163935+RodolfoNieblas@users.noreply.github.com>
2020-05-02 17:47:29 -0700

    Added to Scene01

--- Untitled_Horror_Game/Assets/Scene01/Scripts_Scene01/playerMove.cs
+++ Untitled_Horror_Game/Assets/Scene01/Scripts_Scene01/playerMove.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class placeholder2 : MonoBehaviour
+public class playerMove : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class placeholder2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position += movement * Time.deltaTime * speed;
     }
 }
